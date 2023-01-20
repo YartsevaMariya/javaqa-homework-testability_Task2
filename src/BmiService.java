0888768d5dab241976e0084975870605e11d7c8d
Yartseva Maria <kammariya@mail.ru>
2023-01-20 16:40:56 +0300

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,7 @@
+public class BmiService {
+    public int calculate(int weight, int height) {
+        int index = weight / (height * height);
+
+        return index;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int bmi = service.calculate(90,2);
+        System.out.println(bmi);
+
+
+    }
+}
